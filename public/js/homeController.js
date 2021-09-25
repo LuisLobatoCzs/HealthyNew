603d9d813d8709ad52376b8f7ec6d5a5fe5be8c7
Marigoislas <marigoislas@gmail.com>
2021-09-24 20:19:52 -0500

    ayuda tablet y celular

--- public/js/homeController.js
+++ public/js/homeController.js
@@ -93,86 +93,86 @@ var app = angular.module("home", [])
                 break;
             case 15:
                 Swal.fire(
-                    'Pregunta?',
-                    'Respuesta',
+                    '¿Cómo me registro para ser nuevo usuario?',
+                    'Registrarte como usuario en Health& es muy sencillo Primero, agrega la forma de registro por la que te quieras dar de alta en la app y acepta los términos y condiciones (no olvides leerlos bien antes de aceptarlos). Una vez finalizado este paso llenarás un formulario con algunos datos importantes como contraseña y aspectos básicos de tu perfil. Finalmente te pediremos que nos compartas tus datos e información de pago y ¡Ya estás listo para recibir consultas Health&!',
                 );
                 break;
             case 16:
                 Swal.fire(
-                    'Pregunta?',
-                    'Respuesta',
+                    '¿Cómo puedo cambiar la información en mi perfil?',
+                    'Puedes cambia la información de tu perfil haciendo clic en el botón “mi perfil“ y luego en “cuenta”, aquí podrás editar: Foto de perfil, Nombre Completo, Número de Teléfono asociado a tu cuenta, Correo Electrónico y Contraseña.',
                 );
                 break;
             case 17:
                 Swal.fire(
-                    'Pregunta?',
-                    'Respuesta',
+                    '¿Cómo agrego un contacto de emergencia?',
+                    'Para ingresar a tus contactos de emergencia da clic en el botón “mi perfil” después, selecciona “contactos de emergencia”, posteriormente clic en “añadir contacto de emergencia” y regístralos. En caso de querer actualizar los contactos establecidos, puedes borrar el contacto previo y dar de alta el nuevo.',
                 );
                 break;
             case 18:
                 Swal.fire(
-                    'Pregunta?',
-                    'Respuesta',
+                    '¿Se realiza algún cargo de Pre-autorización?',
+                    '  Por seguridad de las cuentas la plataforma realiza un cargo previo de autorización. Este cargo no es válido y será re integrado a tu cuenta de manera inmediata, sin embargo, puede que este reembolso tarde de 7 a 10 días en verse reflejado en tu cuenta, en caso de que no sea así, te recomendamos que contactes a tu banco para que puedas recibir asesoría.',
                 );
                 break;
             case 19:
                 Swal.fire(
-                    'Pregunta?',
-                    'Respuesta',
+                    '¿Qué métodos de pago son aceptados en Healthy?',
+                    'Por el momento aceptamos todas las tarjetas de crédito y débito Visa y MasterCard. ',
                 );
                 break;
             case 20:
                 Swal.fire(
-                    'Pregunta?',
-                    'Respuesta',
+                    '¿Cómo se realiza el cobro del servicio?',
+                    'Antes de poder realizar alguna cita con nuestros especialistas, primero debes dar de alta una tarjeta de crédito en la aplicación, una vez que se da por finalizada la consulta se realiza el cobro. En Healthy es primordial que puedas tener una consulta tranquila y segura. ',
                 );
                 break;
             case 21:
                 Swal.fire(
-                    'Pregunta?',
-                    'Respuesta',
+                    '¿Puedo programar una cita con algún especialista?',
+                    'Por el momento Healthy no cuenta con esta función, ya que buscamos que nuestros especialistas tengan la libertar de poder elegir cuándo y cómo trabajar en la aplicación, sin embargo, puedes pedir una consulta en el momento que desees y con el especialista que desees. ',
                 );
                 break;
             case 22:
                 Swal.fire(
-                    'Pregunta?',
-                    'Respuesta',
+                    '¿Puedo dar propinas?',
+                    ' ¡Por supuesto! Si deseas darle una propina al especialista puedes hacerlo por medio de la app al terminar tu consulta califica a tu especialista y posterior selecciona la cantidad de propina que deseas añadir. Recuerda que NO ES OBLIGATORIO y no pueden cobrar más que el monto por el que solicitaste el servicio, si tu especialista te solicitó propina o dinero en efectivo, por favor repórtalo inmediatamente a Soporte Healthy',
                 );
                 break;
             case 23:
                 Swal.fire(
-                    'Pregunta?',
-                    'Respuesta',
+                    '¿Cómo solicito un servicio de algún especialista?',
+                    'Al momento de ingresar a la app veras en la parte superior tres esferas (médico general, fisioterapeuta y enfermera) da clic en alguna de ella acorde con tus necesidades, si no te encuentras seguro de que especialista es el adecuado para tu necesidad, puedes escribir el motivo de consulta y la app te direccionará al especialista que necesites. Posteriormente, en la parte inferior aparecerá el monto aproximado del cobro y los especialistas que se encuentran disponibles, podrás elegir la ubicación en donde necesites el servicio. Si el servicio es requerido para otra persona tendrás que especificarlo. Por último, confirma tu consulta y listo, el especialista llegará hasta la ubicación brindada.',
                 );
                 break;
             case 24:
                 Swal.fire(
-                    'Pregunta?',
-                    'Respuesta',
+                    '¿Cómo puedo identificar a mi especialista?',
+                    'Al pedir tu consulta, te aparecerá un recuadro con los datos delespecialista y ubicación que te ayudaran a identificarlo con facilidad: 1. Nombre. 2. Foto. 3. Cedula profesional. Antes de iniciar la consulta el especialista se presentará contigo, asegúrate que los datos proporcionados coincidan con los que se indican en la app. Si ves que alguno de los datos no concuerda, cancela la consulta, eligiendo opción asociada y contacta a Soporte Healthy.',
                 );
                 break;
             case 25:
                 Swal.fire(
-                    'Pregunta?',
-                    'Respuesta',
+                    '¿Qué debo hacer si se realizó un cobro sin recibir el servicio?',
+                    'De parte del equipo Healthy te ofrecemos una disculpa por esto. Entra a tu app y en el menú da clic en el botón “Mi perfil”, selecciona Registro de Consultas y da clic en la consulta donde sucedió el percance, por último, elige “Soporte Técnico” y el equipo de Healthy te ofrecerá la mejor solución.',
                 );
                 break;
             case 26:
                 Swal.fire(
-                    'Pregunta?',
-                    'Respuesta',
+                    ' ¿Cuenta con tarifa dinámica? ',
+                    'Si, contamos con Tarifa dinámica en nuestras consultas, pero podrás consultar si tu cita se encuentra afectada por este motivo. ',
                 );
                 break;
             case 27:
                 Swal.fire(
-                    'Pregunta?',
-                    'Respuesta',
+                    '¿Puedo pedir Healthy para algún conocido o familiar?',
+                    '¡Si es posible! Antes de confirmar la consulta puedes seleccionar si el servicio es para ti o para alguien más. Este paso es importante ya que el especialista tiene prohibido brindar el servicio para alguien más que no sea el usuario que lo solicitó, esto por motivos de seguridad, sugerimos que tu conocido también descargue la app en caso de ser mayor de edad. ¡Recuerda que refiriendo a tu conocido, ambos recibirán beneficios!',
                 );
                 break;
             case 28:
                 Swal.fire(
-                    'Pregunta?',
-                    'Respuesta',
+                    '¿Qué hacer en caso de emergencia?',
+                    'Si te encuentras en una emergencia durante la consulta en Healthy: Da clic al icono del estetoscopio con escudo localizado en la parte inferior izquierda de la pantalla principal de tu app. Este botón abre el menú de seguridad, selecciona “911” para que enlace una línea directa con un equipo especializado que te brindaran apoyo.',
                 );
                 break;
         }
@@ -258,74 +258,74 @@ var app = angular.module("home", [])
                 break;
             case 13:
                 Swal.fire(
-                    'Pregunta?',
-                    'Respuesta',
+                    '¿Qué documentos necesito para convertirme en un especialista en Healthy?',
+                    'Para ser parte de nuestra comunidad de usuario Especialista, necesitarás:	Cartilla de vacunación COVID,	Identificación oficial vigente (INE),	Cédula profesional, Certificado de no antecedentes penales federales (de no más de 3 meses de haber sido expedido), CURP,	Licencia de conducir (en caso de registrar algún vehículo o moto) *Esta información puede cambiar en conformidad con las legislaciones locales.',
                 );
                 break;
             case 14:
                 Swal.fire(
-                    'Pregunta?',
-                    'Respuesta',
+                    '¿Qué áreas de especialistas se encuentran disponibles?',
+                    'Healthy cuenta 3 áreas de especialistas en el área de salud,	Medicina General,	Fisioterapia,	Enfermería, Escoge en la que te desempeñaras y se parte de esta gran comunidad hoy.',
                 );
                 break;
             case 15:
                 Swal.fire(
-                    'Pregunta?',
-                    'Respuesta',
+                    'Tengo una discapacidad. ¿Puedo usar Healthy?',
+                    'En Healthy la inclusión es uno de nuestros pilares principales, por eso sin importar tu discapacidad si eres un profesional y pasas los filtros de seguridad puedes ser parte de nuestro equipo. ¿Tienes dudas puntuales? Puede contactar con el Soporte de Healthy.',
                 );
                 break;
             case 16:
                 Swal.fire(
-                    'Pregunta?',
-                    'Respuesta',
+                    'Tengo Problemas con el registro',
+                    'Pueden existir diversas razones por las cuales presentes problemas con el registro, para poder apoyarte con la mejor solución, contacta a Soporte Healthy.',
                 );
                 break;
             case 17:
                 Swal.fire(
-                    'Pregunta?',
-                    'Respuesta',
+                    '¿Cómo se calculan las tarifas en Healthy?',
+                    'Todas nuestras tarifas se calculan con base en el horario en el que se solicite el servicio además de la comisión de los especialistas.  ',
                 );
                 break;
             case 18:
                 Swal.fire(
-                    'Pregunta?',
-                    'Respuesta',
+                    'No puedo terminar el servicio',
+                    'Haz clic en el botón “Guía” para acceder de forma rápida al “registro de consultas”, luego ve a “ayuda” y selecciona la opción que mejor se adapte a tu problema.',
                 );
                 break;
             case 19:
                 Swal.fire(
-                    'Pregunta?',
-                    'Respuesta',
+                    '¿Cómo acepto una consulta?',
+                    'Una vez que llegue la alerta de servicio, aparecerá en tu pantalla el motivo de consulta y la dirección del paciente, en la parte inferior de esta pestaña verás la opción de “Aceptar” o “Rechazar” de acuerdo con la acción que desees realizar.',
                 );
                 break;
             case 20:
                 Swal.fire(
-                    'Pregunta?',
-                    'Respuesta',
+                    '¿Cómo puedo identificar a mi usuario cuando me encuentre en el domicilio?',
+                    'Una vez que te encuentras en el domicilio corrobora los datos proporcionados por el usuario, ya que este puede solicitar el servicio para otra persona. Por tu seguridad, siempre confirma que el usuario sea el que realizó la solicitud.',
                 );
             break;
             case 21:
                 Swal.fire(
-                    'Pregunta?',
-                    'Respuesta',
+                    '¿Dónde puedo encontrar mi historial de consultas?',
+                    'Para encontrar tu historial de consultas: haz clic en “Guía” y selecciona “Registro de consultas”. Por seguridad del Usuario los registros se eliminan cada semana. ',
                 );
             break;
             case 22:
                 Swal.fire(
-                    'Pregunta?',
-                    'Respuesta',
+                    'Tuve una mala experiencia en una consulta',
+                    'Para reportar una mala experiencia con la aplicación o con alguno de nuestros expertos haz clic en “Guía” y selecciona “Registro de consultas”, después haz clic en la consulta donde ocurrió el problema y repórtalo. ',
                 );
                 break;
             case 23:
                 Swal.fire(
-                    'Pregunta?',
-                    'Respuesta',
+                    '¿Qué debo hacer en caso de Emergencia?',
+                    'Durante una emergencia, debes presionar el botón de seguridad que se encuentra en la pantalla principal. Este botón abre una línea directa con el 911 (línea de emergencia en el estado de Puebla) donde podrás exponer tu situación.',
                 );
                 break;
             case 24:
                 Swal.fire(
-                    'Pregunta?',
-                    'Respuesta',
+                    '¿Cómo agrego un contacto de emergencia?',
+                    'Para agregar un contacto de emergencia haz clic en “Guía”, selecciona “Mi Perfil” y presiona el botón “Contacto de emergencia”, ahí podrás incluir o editar la información de tus contactos de emergencia.',
                 );
                 break;
         }
